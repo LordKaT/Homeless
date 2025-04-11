15da27b9ab630c4232eac9fbcaa027b9d5a0c2ce
Felicia Mirabel <lordkat@lordkat.com>
2025-04-11 16:48:41 -0400

    Hello

--- pyscript/core.js
+++ pyscript/core.js
@@ -1,2 +1,2 @@
-export{u as MPWorker,t as PyWorker,T as TYPES,c as codemirror,v as config,q as donkey,e as hooks,i as inputFailure,o as offline_interpreter,p as optional,r as relative_url,s as stdlib,w as whenDefined}from"./core-CxQj1agm.js";
+export{u as MPWorker,t as PyWorker,T as TYPES,c as codemirror,v as config,q as donkey,e as hooks,i as inputFailure,o as offline_interpreter,p as optional,r as relative_url,s as stdlib,w as whenDefined}from"./core-DzhD2zJj.js";
 //# sourceMappingURL=core.js.map
